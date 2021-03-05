dddbad1a405302ed6366841d525731ddb112284b
Bradley Cushing <bradleycushing@gmail.com>
2021-03-05 17:12:55 -0500

    build(webpack): use typescript for webpack configs and update settings
    Update tsconfig to compile typescript to ES2017 target. Update webpack
    configurations to use typescript as well as webpack-merge for a more
    declarative merging approach. Use tslib at runtime with importHelpers
    tsconfig setting to reduce helper duplication when bundling. Explicitly
    define types in tsconfig in preparation for upgrading to Yarn 2 and
    using chrome-types. Add and remove a few tsconfig options.

--- meta/webpack.config.dev.js
+++
@@ -1,9 +0,0 @@
-const makeBaseConfig = require('./webpack.config.base');
-
-const MODE = 'development';
-
-module.exports = {
-    ...makeBaseConfig({ mode: MODE }),
-    mode: MODE,
-    devtool: 'inline-source-map',
-};
