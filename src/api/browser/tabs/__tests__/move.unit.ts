7dd2aaaf72fe1458208ded4aa22c68db6c1ce4bf
Bradley Cushing <bradleycushing@gmail.com>
2021-04-29 11:08:55 -0400

    test(tabs): test tabs move mapping

---
+++ src/api/browser/tabs/__tests__/move.unit.ts
@@ -0,0 +1,37 @@
+import { move } from '../move';
+import { moveMock } from '../mocks/move';
+
+describe('move', () => {
+    const originalChrome = global.chrome;
+
+    beforeEach(() => {
+        global.chrome = {
+            tabs: {
+                move: moveMock,
+            },
+            runtime: {},
+        } as typeof chrome;
+    });
+    afterEach(() => {
+        global.chrome = originalChrome;
+    });
+
+    it('should resolve after moving a given tab', async () => {
+        global.chrome.runtime.lastError = undefined;
+        const tabId = 1;
+        const moveProperties = { index: -1 };
+        const resolution = await move(tabId, moveProperties);
+        expect(resolution).toBeUndefined();
+    });
+
+    it('should reject with error if one exists', async () => {
+        global.chrome.runtime.lastError = {
+            message: 'error',
+        };
+        const tabId = 1;
+        const moveProperties = { index: -1 };
+        await expect(move(tabId, moveProperties)).rejects.toMatchObject({
+            message: 'error',
+        });
+    });
+});
